cb01189fd01324e47fb0b89987454c20c6e2ae69
Haripriya Mandala <haripriyamandala3@gmail.com>
2026-07-31 10:59:10 +0530

    Time: 3 ms (4.62%), Space: 9.9 MB (80.67%) - LeetHub

---
+++ 1046-last-stone-weight/1046-last-stone-weight.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        priority_queue<int> maxHeap;
+        for(int n:stones){
+            maxHeap.push(n);
+        }
+        while(maxHeap.size()>=2){
+            int largest=maxHeap.top();
+            maxHeap.pop();
+            int second=maxHeap.top();
+            maxHeap.pop();
+            if(largest!=second){
+                maxHeap.push(largest-second);
+            }
+        }
+        return maxHeap.empty()?0:maxHeap.top();
+    }
+};
