2b9bff17299f8f512ba72b201cdd064b6d045fea
Haripriya Mandala <haripriyamandala3@gmail.com>
2026-08-02 22:49:08 +0530

    Time: 159 ms (42.17%), Space: 20.9 MB (15.05%) - LeetHub

---
+++ 0621-task-scheduler/0621-task-scheduler.py
@@ -0,0 +1,16 @@
+class Solution:
+    def leastInterval(self, tasks: List[str], n: int) -> int:
+        count=Counter(tasks)
+        heap=[-freq for freq in count.values()]
+        heapq.heapify(heap)
+        time=0
+        q=deque()
+        while heap or q:
+            time+=1
+            if heap:
+                freq=1+ heapq.heappop(heap)
+                if freq:
+                    q.append((freq, time+n))
+            if q and q[0][1] == time:
+                heapq.heappush(heap, q.popleft()[0])
+        return time
