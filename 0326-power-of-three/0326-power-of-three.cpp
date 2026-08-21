69b06444dd57b26e5ee33d3b9c85a257360dc623
Haripriya Mandala <haripriyamandala3@gmail.com>
2026-08-21 21:34:54 +0530

    Time: 0 ms (100%), Space: 8.7 MB (97.78%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.cpp
+++ 0326-power-of-three/0326-power-of-three.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
     bool isPowerOfThree(int n) {
-        return findAns(n);
-    }
-    bool findAns(int n){
+        if(n<=0) return false;
+        while(n%3==0){
+            n/=3;
+        }
         if(n==1){
             return true;
-        }else if(n<1 || n%3!=0 ){
+        }else{
             return false;
         }
-        
-        return findAns(n/3);
     }
 };
